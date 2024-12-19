ac2885b4e9e3ae462fed0fa327267c89f3f5b057
Udupa776 <udupayashwant@gmail.com>
2024-12-19 23:09:30 +0530

    my first cmmit

--- first.js
+++ first.js
@@ -1,11 +1,2 @@
-let sum=0;
-let marks=[110,222,444,66,28290];
-for (val of marks)
-{
-    dis=val/10;
-    console.log(dis);
-    amt=val-dis;
-    console.log(amt);
-
-}
-console.log(marks);
+let clas=document.querySelectorAll("div");
+console.log(clas);
